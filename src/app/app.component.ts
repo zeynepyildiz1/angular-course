c18d54e410c8e99e4ded074094c712f47189d86b
zeynepyildiz1 <zeyeess@gmail.com>
2020-10-01 19:47:51 +0300

    ImportPackage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-course';
+  constructor(private toastr: ToastrService,
+    private spinner: NgxSpinnerService) {}
+ 
+  ngOnInit(): void {
+    this.showSuccess();
+    this.showSpinner();
+  }
+
+  showSuccess() {
+    this.toastr.success('Angular core projesine hoşgeldiniz.', 'Giriş başarılı!');
+  }
+ showSpinner(){
+  this.spinner.show();
+ 
+  setTimeout(() => {
+    /** spinner ends after 5 seconds */
+    this.spinner.hide();
+  }, 5000);
+ }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// Import library module
+import { NgxSpinnerModule } from "ngx-spinner";
+import { IgxButtonModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      progressBar: true,
+      easing: "ease-in",
+      closeButton: false,
+      progressAnimation: "decreasing",
+      preventDuplicates: true,
+      positionClass: "toast-bottom-left"
+    }),
+    NgxSpinnerModule,
+    IgxButtonModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
